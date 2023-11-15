b62487ba6c3fab1d95fac8689c3a8024832f341d
Zain Lone <zainlone@Zains-MacBook-Pro.local>
2023-11-15 11:31:11 -0500

    removed 0s and aded check for missing values

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,18 +1,36 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import re
 
 df = pd.read_csv("../../data/raw/top_english_movies.csv")
 
+#convert string values with 'K' or 'M' to numeric
 def convert_votes_to_numeric(votes_str):
+    if isinstance(votes_str, float):
+        return int(votes_str)
     multiplier = 1
     if 'K' in votes_str:
         multiplier = 1000
     elif 'M' in votes_str:
         multiplier = 1000000
-    return float(re.sub(r'[^0-9.]', '', votes_str)) * multiplier
+    return int(float(re.sub(r'[^0-9.]', '', votes_str)) * multiplier)
 
 df['user_votes'] = df['user_votes'].apply(convert_votes_to_numeric)
 
 print(df.head())
+
+
+#check for missing values
+missing_values = df.isnull().sum()
+
+print("Missing Values:\n", missing_values)
+
+percentage_missing = (missing_values / len(df)) * 100
+print("\nPercentage of Missing Values:\n", percentage_missing)
+
+#display the rows that are missing
+missing_rows = df[df.isnull().any(axis=1)]
+print("Rows with Missing Values:\n", missing_rows)
+
