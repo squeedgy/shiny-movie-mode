8a0874482590adbc3040dd043821ec36c7133161
Zain Lone <zainlone@Zains-MacBook-Pro.local>
2023-11-15 12:07:21 -0500

    plot for top 5 movies

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,6 +6,8 @@ import re
 
 df = pd.read_csv("../../data/raw/top_english_movies.csv")
 
+print("Data Types:\n", df.dtypes)
+
 #convert string values with 'K' or 'M' to numeric
 def convert_votes_to_numeric(votes_str):
     if isinstance(votes_str, float):
@@ -21,6 +23,7 @@ df['user_votes'] = df['user_votes'].apply(convert_votes_to_numeric)
 
 print(df.head())
 
+df_aggregated = df.resample('Y').mean()
 
 #check for missing values
 missing_values = df.isnull().sum()
@@ -34,3 +37,14 @@ print("\nPercentage of Missing Values:\n", percentage_missing)
 missing_rows = df[df.isnull().any(axis=1)]
 print("Rows with Missing Values:\n", missing_rows)
 
+# Group by movie name, summing the user votes
+top_movies = df.groupby('movie_name')['user_votes'].sum().sort_values(ascending=False).head(5)
+
+# Plotting the top 5 movies based on user votes
+plt.figure(figsize=(12, 6))
+sns.barplot(x=top_movies.values, y=top_movies.index, palette="viridis")
+plt.title('Top 5 Movies Based on User Votes')
+plt.xlabel('Total User Votes')
+plt.ylabel('Movie Name')
+plt.tight_layout()  #adjust layout so text fits
+plt.show()
